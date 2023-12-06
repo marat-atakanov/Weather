3a5b3341041347703cbc1eac90dd4e1a8bb9cb85
marat-atakanov <marat.kg02@gmail.com>
2023-12-06 17:15:51 +0600

    weather project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState("Bishkek")
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&untis=metric&lang=ru&appid=5a66bf1c19fe24223d418a534734a2cc`
+
+    const searchLocation = (event) => {
+        console.log(event.key);
+        if (event.key === "Enter") {
+            axios.get(url).then((response) => {
+                setData(response.data)
+                console.log(response.data);
+            })
+            setLocation("");
+            console.log(data)
+        }
+    }
+
+    return (
+        <div className="app">
+            <div className="search">
+                <input
+                    type="text"
+                    value={location}
+                    onChange={(event) => {
+                        setLocation(event.target.value)
+                    }}
+                    onKeyPress={searchLocation}
+                    placeholder="Введите название"
+                />
+            </div>
+            <div className="container">
+                <div className="top">
+                    <div className="location">
+                        <p>
+                            {data.name}
+                        </p>
+                    </div>
+                    <div className="temp">
+                        {data.main ? <h1>{data.main.temp.toFixed() - 273} {'\u00b0'}C</h1> : null}
+                    </div>
+                    <div className="description">
+                        {data.weather ? <p>{data.weather[0].description}</p> : null}
+                    </div>
+                </div>
+
+                {data.name !== undefined && (
+                    <div className="bottom">
+                        <div className="feels">
+                            {data.main ? (
+                                <p className="bold">{data.main.feels_like.toFixed() - 273} {'\u00b0'}C</p>
+                            ) : null}
+                            <p>Чувствуется как</p>
+                        </div>
+                        <div className="humidity">
+                            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+                            <p>Влажность</p>
+                        </div>
+                        <div className="wind">
+                            {data.wind ? (
+                                <p className="bold">{data.wind.speed.toFixed()} м/с</p>
+                            ) : null}
+                            <p>Скрость ветра</p>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default App;
